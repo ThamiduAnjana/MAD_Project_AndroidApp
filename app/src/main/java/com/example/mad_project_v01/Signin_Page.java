5e70309065049c9c81eaaf2a4ba61cfe72f78649
ThamiduAnjana <pmatabandara@students.nsbm.lk>
2020-12-03 22:49:58 +0530

    Add project files

---
+++ app/src/main/java/com/example/mad_project_v01/Signin_Page.java
@@ -0,0 +1,14 @@
+package com.example.mad_project_v01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signin_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin__page);
+    }
+}
